f4d0bdee57ce042264aa355586861863e5c87cc7
Shubham Bairagi <sbairagi825@gmail.com>
2023-11-26 18:39:27 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'portfolio';
+  // title = 'portfolio';
+  // toggleMobile = false;
+  // toggleClass = "bi mobile-nav-toggle d-xl-none bi-list"
+  // bodyClass = ""
+  step = "step1"
+
+  constructor() {
+    
+  }
+
+  // clicked(){
+  //   if (this.toggleMobile == false){
+  //     this.toggleMobile = true;
+  //     this.bodyClass = "mobile-nav-active"
+  //     this.toggleClass = "bi mobile-nav-toggle d-xl-none bi-x"
+  //   }else{
+  //     this.toggleMobile = false;
+  //     this.bodyClass = ""
+  //     this.toggleClass = "bi mobile-nav-toggle d-xl-none bi-list"
+  //   }
+  // }
 }
